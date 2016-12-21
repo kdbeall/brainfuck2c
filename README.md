52cb1da81e1b281885bbd9570936a26ccb137494
Keng-Hui Beall <kdbeall@ncsu.edu>
2016-12-21 12:53:22 +0900

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Brainfuck2C
-Simple Brainfuck to C Source to Source Compiler Written in C
+A simple Brainfuck to C source to source compiler written in C.
+In other words, this program converts Brainfuck source code to C source code.
+The program also checks for mismatched curly braces and indents the outputted C code.
+
+To use this program, first clone it from this Github repository.
+I'll assume you're using Linux and gcc.
+
+Open up your terminal and do the following in a directory of your choice.
+
+```shell
+git clone https://github.com/kdbeall/Brainfuck2C.git
+```
+
+Next, *cd* into the newly created Brainfuck2C directory.
+
+```shell
+cd Brainfuck2C
+```
+
+Next, let's build the program.
+There's a Makefile ready to be used.
+
+```shell
+make
+```
+
+Our program takes a Brainfuck source file .bf and outputs a .c source file.
+It takes the two file names as commandline arguments.
+
+```shell
+./brainfuck2c [Your Brainfuck source file] [Your output C source file]
+```
+
+I've included a test folder with a single test file.
+To run the hello world example, do the following.
+
+```shell
+./brainfuck2c ./test/helloworld.bf ./test/helloworld.c
+```
+
+Next compile the helloworld.c file with gcc.
+```shell
+gcc -Wall -std=c99 helloworld.c -o helloworld
+```
+
+Last, but not least, run it.
+
+```shell
+./helloworld
+```
+
+
--- test/helloworld.c
+++ test/helloworld.c
@@ -1,217 +1,218 @@
 int main()
 {
-char array[30000] = {0};
-char *ptr = array;
-++*ptr;
+  char array[30000] = { 0 };
+  char *ptr = array;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-while (*ptr) {
+  while (*ptr) {
 
-++ptr;
+    ++ptr;
 
-++*ptr;
+    ++*ptr;
 
-++*ptr;
+    ++*ptr;
 
-++*ptr;
+    ++*ptr;
 
-++*ptr;
+    ++*ptr;
 
-while (*ptr) {
+    while (*ptr) {
 
-++ptr;
+      ++ptr;
 
-++*ptr;
+      ++*ptr;
 
-++*ptr;
+      ++*ptr;
 
-++ptr;
+      ++ptr;
 
-++*ptr;
+      ++*ptr;
 
-++*ptr;
+      ++*ptr;
 
-++*ptr;
+      ++*ptr;
 
-++ptr;
+      ++ptr;
 
-++*ptr;
+      ++*ptr;
 
-++*ptr;
+      ++*ptr;
 
-++*ptr;
+      ++*ptr;
 
-++ptr;
+      ++ptr;
 
-++*ptr;
+      ++*ptr;
 
---ptr;
+      --ptr;
 
---ptr;
+      --ptr;
 
---ptr;
+      --ptr;
 
---ptr;
+      --ptr;
 
---*ptr;
+      --*ptr;
 
-}
+    }
 
-++ptr;
+    ++ptr;
 
-++*ptr;
+    ++*ptr;
 
-++ptr;
+    ++ptr;
 
-++*ptr;
+    ++*ptr;
 
-++ptr;
+    ++ptr;
 
---*ptr;
+    --*ptr;
 
-++ptr;
+    ++ptr;
 
-++ptr;
+    ++ptr;
 
-++*ptr;
+    ++*ptr;
 
-while (*ptr) {
+    while (*ptr) {
 
---ptr;
+      --ptr;
 
-}
+    }
 
---ptr;
+    --ptr;
 
---*ptr;
+    --*ptr;
 
-}
+  }
 
-++ptr;
+  ++ptr;
 
-++ptr;
+  ++ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++ptr;
+  ++ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++ptr;
+  ++ptr;
 
-++ptr;
+  ++ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
---ptr;
+  --ptr;
 
---*ptr;
+  --*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
---ptr;
+  --ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
---*ptr;
+  --*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++ptr;
+  ++ptr;
 
-++ptr;
+  ++ptr;
 
-++*ptr;
+  ++*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
 
-++ptr;
+  ++ptr;
 
-++*ptr;
+  ++*ptr;
 
-++*ptr;
+  ++*ptr;
 
-putchar(*ptr);
+  putchar(*ptr);
+
 
 }
